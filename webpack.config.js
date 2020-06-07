1d801b2333766db170bee07a82a93b2960596902
sidiousvic <victor6rene@gmail.com>
2020-06-07 17:09:03 +0900

    Webpack + TS configs

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/App.tsx",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    // hot: true
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "museo",
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+    ],
+  },
+};
